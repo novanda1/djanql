847a974a59916a6d92afec0a5bc53e3def0830b2
Novanda Ahsan <novandaahsan1@gmail.com>
2021-10-05 10:11:27 +0700

    next: infinite pagination for PostList

--- apps/next/src/components/Post/PostList.tsx
+++ apps/next/src/components/Post/PostList.tsx
@@ -3,14 +3,34 @@ import { usePostsQuery } from '../../generated/graphql';
 import { SinglePostList } from './SinglePostList';
 
 export const PostList: React.FC = () => {
-  const { data, loading } = usePostsQuery();
+  const { data, loading, fetchMore } = usePostsQuery({
+    variables: {
+      postsLimit: 3,
+    },
+  });
 
   return (
     <div className="mt-6 grid gap-12 border-t-2 border-gray-100 py-8">
       {loading ? 'loading...' : ''}
-      {data?.posts.map((post) => (
+      {data?.posts.result.map((post) => (
         <SinglePostList key={post.id} data={post} />
       ))}
+
+      {data?.posts.hasNext && (
+        <>
+          <button
+            onClick={() => {
+              fetchMore({
+                variables: {
+                  after: data?.posts.nextCursor,
+                },
+              });
+            }}
+          >
+            Load More
+          </button>
+        </>
+      )}
     </div>
   );
 };
--- apps/next/src/lib/withApollo.ts
+++ apps/next/src/lib/withApollo.ts
@@ -1,13 +1,32 @@
-import { withApollo } from 'next-apollo';
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import { withApollo } from 'next-apollo';
+import { PostWCursor } from '../generated/graphql';
 
 const apolloClient = new ApolloClient({
   ssrMode: typeof window === 'undefined',
   link: new HttpLink({
-    uri: 'http://localhost:9090/graphql/', // Server URL (must be absolute)
-    credentials: 'same-origin', // Additional fetch() options like `credentials` or `headers`
+    uri: 'http://localhost:9090/graphql/',
+    credentials: 'same-origin',
   }),
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          posts: {
+            keyArgs: [],
+            merge(existing: PostWCursor, incoming: PostWCursor) {
+              if (existing)
+                return {
+                  ...incoming,
+                  result: [...existing.result, ...incoming.result],
+                };
+              else return incoming;
+            },
+          },
+        },
+      },
+    },
+  }),
 });
 
 export default withApollo(apolloClient);
